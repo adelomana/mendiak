fb33b762726b90c5f9921d229ce34a3efa983b2b
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2016-12-12 21:45:04 -0800

    submitting working version of consistentGenesExpression

--- consistentGenesExpression.py
+++ consistentGenesExpression.py
@@ -4,7 +4,7 @@
 
 import pickle,sys,numpy
 import matplotlib,matplotlib.pyplot
-import scipy,scipy.stats
+import scipy,scipy.stats,scipy.interpolate
 
 def expressionReader():
     
@@ -28,73 +28,85 @@ def expressionReader():
 ###
 
 # 0. user defined variables
-jarDir='/Volumes/omics4tb/alomana/projects/csp.jgi/results/jars.wk/'
+jarDir='/Volumes/omics4tb/alomana/projects/csp.jgi/results/jars/'
 consistentGenesJar=jarDir+'consistentGenes.filtered.pickle'
 expressionDataFile='/Volumes/omics4tb/alomana/projects/csp.jgi/data/expression/matrix.19506.v.5.5.FPKM.txt'
 iterations=100
+figuresDir='/Volumes/omics4tb/alomana/projects/csp.jgi/results/figures/'
 
 # 1. reading data
+print('reading data...')
 
 # 1.1. reading signature genes
 f=open(consistentGenesJar,'rb')
 consistentGenes=pickle.load(f)
 f.close()
 
-for key in consistentGenes.keys():
-    print(key)
-    print(len(consistentGenes[key]))
-    print(consistentGenes[key][:10])
-    print()
-sys.exit()
-
 # 1.2. reading expression data at 24 h
 expression=expressionReader()
 
 # 2. compute distribution of expression for signature genes
-signatureExpression=[]
-for gene in genesAllSignature01:
-    fpkm=expression[gene]
-    signatureExpression.append(numpy.log10(fpkm+1))
+print('retrieving distribution of signature genes...')
 
-# 4. plot figure
+genes24h=consistentGenes[('24hA', '24hB')]
+expression24h=[]
+for gene in genes24h:
+    fpkm=expression[gene]
+    expression24h.append(numpy.log10(fpkm+1))
+
+# 3. computing statistical difference
 print('computing statistical analysis...')
 
 # working with signature genes
-probS,binEdges=numpy.histogram(signatureExpression,bins='auto',density=True)
+probS,binEdges=numpy.histogram(expression24h,bins='auto',density=True)
 cumsumSignature=numpy.cumsum(probS)/sum(probS)
 halfBin=binEdges[1]/2
 x=[element+halfBin for element in binEdges]
 x.pop()
-matplotlib.pyplot.plot(x,cumsumSignature,'or',mew=0)
+
+matplotlib.pyplot.plot(x,cumsumSignature,'or',mew=0,alpha=0.5)
+model=scipy.interpolate.PchipInterpolator(x,cumsumSignature)
+newTime=numpy.arange(min(x),max(x),(max(x)-min(x))/100)
+inter=model(newTime)
+matplotlib.pyplot.plot(newTime,inter,'-r',lw=2)
 
 # working with background genes
 allGenes=list(expression.keys())
-otherSideGenes=[gene for gene in allGenes if gene not in genesAllSignature01]
+otherSideGenes=[gene for gene in allGenes if gene not in expression24h]
 
-backgroundHistogram=[]
-
+backgroundDist=[]
 for i in range(iterations):
-    sampledGenes=numpy.random.choice(otherSideGenes,size=len(genesAllSignature01))
+    sampledGenes=numpy.random.choice(otherSideGenes,size=len(expression24h))
     v=[]
     for gene in sampledGenes:
         fpkm=expression[gene]
         v.append(numpy.log10(fpkm+1))
-    prob,binEdges=numpy.histogram(v,bins=binEdges,density=True)
-    backgroundHistogram.append(prob)
-
-    print(scipy.stats.ks_2samp(signatureExpression,v))
-
+    v.sort()
+    backgroundDist.append(v)
+    ks,pvalue=scipy.stats.ks_2samp(expression24h,v)
+    print('\t iteration {}, ks={}, p-value={}'.format(i,ks,pvalue))
 print()
 
-sys.exit()
+background=numpy.array(backgroundDist)
+meanBackground=numpy.mean(background,axis=0)
+ksF,pvalueF=scipy.stats.ks_2samp(expression24h,meanBackground)
+print('\t against mean background distribution: ks={}, p-value={}'.format(ks,pvalue))
 
-allProb=numpy.array(backgroundHistogram)
-meanProb=numpy.mean(allProb,axis=0)
+probB,tempo=numpy.histogram(meanBackground,bins=binEdges,density=True)
+cumsumBackground=numpy.cumsum(probB)/sum(probB)
 
-cumsumBackground=numpy.cumsum(meanProb)/sum(meanProb)
-matplotlib.pyplot.plot(x,cumsumBackground,'ok',mew=0)
+matplotlib.pyplot.plot(x,cumsumBackground,'ok',mew=0,alpha=0.5)
+model=scipy.interpolate.PchipInterpolator(x,cumsumBackground)
+newTime=numpy.arange(min(x),max(x),(max(x)-min(x))/100)
+inter=model(newTime)
+matplotlib.pyplot.plot(newTime,inter,'-k',lw=2)
 
-# statistical difference
-print(scipy.stats.ks_2samp(probS,meanProb))
+matplotlib.pyplot.xlim([-0.1,4.1])
+matplotlib.pyplot.ylim([-0.05,1.05])
 
+matplotlib.pyplot.xlabel('expression, $e$ (log$_2$(FPKM+1))')
+matplotlib.pyplot.ylabel('CDF (P($E \leq e$)')
+
+figureFile=figuresDir+'pdf.24h.pdf'
 matplotlib.pyplot.savefig('thefigure.pdf')
+#matplotlib.pyplot.savefig(figureFile)
